c3e8248c40301c4f2725ff38f0dc4edffb38b20f
pbenikovszky <pbenikovszky@gmail.com>
2020-06-05 02:46:42 +0200

    COC API upload

---
+++ CocAPITest/CocAPITest/Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace CocAPITest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGetData = new System.Windows.Forms.Button();
+            this.txtTag = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtLocation = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtType = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtClanLevel = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtClanPoints = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtWarWins = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnGetData
+            // 
+            this.btnGetData.Enabled = false;
+            this.btnGetData.Location = new System.Drawing.Point(234, 34);
+            this.btnGetData.Name = "btnGetData";
+            this.btnGetData.Size = new System.Drawing.Size(75, 20);
+            this.btnGetData.TabIndex = 0;
+            this.btnGetData.Text = "GET";
+            this.btnGetData.UseVisualStyleBackColor = true;
+            this.btnGetData.Click += new System.EventHandler(this.btnGetData_Click);
+            // 
+            // txtTag
+            // 
+            this.txtTag.Location = new System.Drawing.Point(128, 34);
+            this.txtTag.Name = "txtTag";
+            this.txtTag.Size = new System.Drawing.Size(100, 20);
+            this.txtTag.TabIndex = 2;
+            this.txtTag.TextChanged += new System.EventHandler(this.txtTag_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(44, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Clan tag";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(47, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Name";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(128, 69);
+            this.txtName.Name = "txtName";
+            this.txtName.ReadOnly = true;
+            this.txtName.Size = new System.Drawing.Size(181, 20);
+            this.txtName.TabIndex = 5;
+            // 
+            // txtLocation
+            // 
+            this.txtLocation.Location = new System.Drawing.Point(128, 95);
+            this.txtLocation.Name = "txtLocation";
+            this.txtLocation.ReadOnly = true;
+            this.txtLocation.Size = new System.Drawing.Size(181, 20);
+            this.txtLocation.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(47, 99);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Location";
+            // 
+            // txtType
+            // 
+            this.txtType.Location = new System.Drawing.Point(128, 121);
+            this.txtType.Name = "txtType";
+            this.txtType.ReadOnly = true;
+            this.txtType.Size = new System.Drawing.Size(181, 20);
+            this.txtType.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(47, 125);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Type";
+            // 
+            // txtClanLevel
+            // 
+            this.txtClanLevel.Location = new System.Drawing.Point(128, 147);
+            this.txtClanLevel.Name = "txtClanLevel";
+            this.txtClanLevel.ReadOnly = true;
+            this.txtClanLevel.Size = new System.Drawing.Size(181, 20);
+            this.txtClanLevel.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(47, 151);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Clan level";
+            // 
+            // txtClanPoints
+            // 
+            this.txtClanPoints.Location = new System.Drawing.Point(128, 173);
+            this.txtClanPoints.Name = "txtClanPoints";
+            this.txtClanPoints.ReadOnly = true;
+            this.txtClanPoints.Size = new System.Drawing.Size(181, 20);
+            this.txtClanPoints.TabIndex = 13;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(47, 177);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(59, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Clan points";
+            // 
+            // txtWarWins
+            // 
+            this.txtWarWins.Location = new System.Drawing.Point(128, 199);
+            this.txtWarWins.Name = "txtWarWins";
+            this.txtWarWins.ReadOnly = true;
+            this.txtWarWins.Size = new System.Drawing.Size(181, 20);
+            this.txtWarWins.TabIndex = 15;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(47, 203);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(51, 13);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "War wins";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(349, 230);
+            this.Controls.Add(this.txtWarWins);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtClanPoints);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtClanLevel);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtType);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtLocation);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtTag);
+            this.Controls.Add(this.btnGetData);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "COC API Test";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGetData;
+        private System.Windows.Forms.TextBox txtTag;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtLocation;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtType;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtClanLevel;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtClanPoints;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtWarWins;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ CocAPITest/CocAPITest/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows.Forms;
+using CocNET;
+using CocNET.Interfaces;
+
+namespace CocAPITest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGetData_Click(object sender, EventArgs e)
+        {
+            string token =
+                "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjMwMzc4M2ViLWIyNWYtNDU5MC05M2NmLTFhNmZjMzQ5Yjk1OSIsImlhdCI6MTU5MTMxNTIyMCwic3ViIjoiZGV2ZWxvcGVyLzcyODgzMmI1LTI4NzgtYTJlZi01MTQ0LTUyNTM5N2U3YzI4NiIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjk0LjIxLjM0LjEwOSJdLCJ0eXBlIjoiY2xpZW50In1dfQ.OgBZEinxO9LZ51E8mgPZKT_UiWsNt6IvZgr-1wNfZZINkv-vQvtN6vNFaTHXIecCao5RwAh3QHOAWjQS_fhulg";
+
+            Funq.Container container = CocCore.Instance(token).Container;
+            ICocCoreClans clansCore = container.Resolve<ICocCoreClans>();
+
+            var clan = clansCore.GetClans(txtTag.Text);
+
+            txtName.Text = clan.Name;
+            txtLocation.Text = clan.Location.Name;
+            txtType.Text = clan.Type;
+            txtClanLevel.Text = clan.ClanLevel.ToString();
+            txtClanPoints.Text = clan.ClanPoints.ToString();
+            txtWarWins.Text = clan.WarWins.ToString();
+
+
+        }
+
+        private void txtTag_TextChanged(object sender, EventArgs e)
+        {
+            btnGetData.Enabled = !String.IsNullOrEmpty(txtTag.Text);
+        }
+    }
+}
